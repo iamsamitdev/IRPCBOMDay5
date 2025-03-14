2ce7f2af9152f4e6e8aa5b44dc188229d534de0c
Samit Koyom <samitkoyom@gmail.com>
2025-03-14 14:00:03 +0700

    update app component

--- FrontendApp/src/app/app.component.ts
+++ FrontendApp/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavComponent } from "./nav/nav.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
