01ee0b172495b67113336699ae28199647b2541c
Samit Koyom <samitkoyom@gmail.com>
2025-03-14 15:57:09 +0700

    update dockerfile

--- FrontendApp/src/app/app.config.ts
+++ FrontendApp/src/app/app.config.ts
@@ -1,13 +1,14 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core'
-import { provideRouter } from '@angular/router'
-import { provideHttpClient } from '@angular/common/http'
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { provideHttpClient, withInterceptors } from '@angular/common/http'
+import { authInterceptor } from './interceptors/auth.interceptor'
 
 import { routes } from './app.routes'
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(withInterceptors([authInterceptor])),
   ]
 }
