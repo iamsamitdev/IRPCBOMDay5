ddd951b3f94aa17e1f021a83c81204b199a0f166
Samit Koyom <samitkoyom@gmail.com>
2025-03-14 11:58:44 +0700

    update update auth.services

--- FrontendApp/src/app/services/auth.service.ts
+++ FrontendApp/src/app/services/auth.service.ts
@@ -1,9 +1,35 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
+import { Observable, tap, catchError, throwError } from 'rxjs'
+import { environment } from '../../environments/environment'
+import { UserModel, UserModelLogin, UserModelRegister, AuthResponse } from '../models/user.model'
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  private apiURL = environment.dotnet_api_url
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  // ฟังก์ชันสำหรับการ Login
+  login(loginData: UserModelLogin): Observable<AuthResponse> {
+    return this.http.post<AuthResponse>(`${this.apiURL}Auth/login`, loginData)
+    .pipe(
+      tap(response => {
+        if(response.success){
+          alert('Login Success')
+        } else {
+          alert('Login Fail')
+        }
+      }),
+      catchError(error => {
+        return throwError(() => error)
+      }
+    ))
+  }
+
 }
